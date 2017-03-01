a26d12a8f6b2e0fe7bd823fc9ab372d4f5b630dd
Eric Vernie <ericv@microsoft.com>
2017-03-01 16:50:37 +0100

    Just hello

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ var builder = require('botbuilder');
 var connector = new builder.ConsoleConnector().listen();
 var bot = new builder.UniversalBot(connector);
 bot.dialog('/', function (session) {
-    session.send('Hello World');
+    session.send("You said: " + session.message.text);
 });
